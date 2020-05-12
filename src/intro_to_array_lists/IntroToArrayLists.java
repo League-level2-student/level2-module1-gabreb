4880b56b87281eed020c8363399649d9d008565b
gabreb <62906858+gabreb@users.noreply.github.com>
2020-05-11 22:00:25 -0700

    hi

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -21,22 +21,21 @@ public class IntroToArrayLists {
 			System.out.println(h);
 		}
 		//4. Print all the Strings using a for-each loop
-				for (int i = 0; i < stuff.size(); i++) {
-					if ((i/2)==0) {
-					System.out.println(stuff.get(i));
-					}
-				}
-		//5. Print only the even numbered elements in the list.
-		for (int i = stuff.size(); i > 0; i++) {
-			System.out.println(stuff.get(i));
-		}
-		for (int i = 0; i < stuff.size(); i++) {
-			for (int x = 0; x < stuff.get(i); x++) {
 				
-			}
-		}
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+				for (int i = stuff.size(); i > 0; i--) {
+					System.out.println(stuff.get(i));
+				}
+				
+				//6. Print all the Strings in reverse order.
+				//5. Print only the even numbered elements in the list.
+						for (int i = 0; i < stuff.size(); i++) {
+							if ((i/2)==0) {
+							System.out.println(stuff.get(i));
+							}
+						}
+				//7. Print only the Strings that have the letter 'e' in them.
+						for (int i = 0; i < stuff.size(); i++) {
+							System.out.println(stuff.get(i).charAt(i));
+									}
 	}
 }
